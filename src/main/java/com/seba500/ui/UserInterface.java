1720170056e6d088c9378b62ec91c381909005c9
Wozniak <wozniak500.com>
2023-06-28 16:05:01 +0200

    log4j - test

--- src/main/java/com/seba500/ui/UserInterface.java
+++ src/main/java/com/seba500/ui/UserInterface.java
@@ -1,7 +1,15 @@
 package com.seba500.ui;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class UserInterface {
   public static void main(String[] args) {
     System.out.println("It Works :)");
+    logger.trace("We've just greeted the user!");
+    logger.debug("We've just greeted the user!");
+    logger.info("We've just greeted the user!");
+    logger.warn("We've just greeted the user!");
+    logger.error("We've just greeted the user!");
+    logger.fatal("We've just greeted the user!");
   }
 }
